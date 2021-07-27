b60a237a360d322a242ba1250892e8b651ebd979
Gruja99 <59265712+Gruja99@users.noreply.github.com>
2021-07-27 15:48:24 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 import router from "./route";
-// create model
-
 // connect mongodb
 mongoose.connect("mongodb://localhost:27017/", {
   useNewUrlParser: true,
